b6c93761ed3c3ba0c4fa584a7e482cde37b85b5b
alex-lairan <lairana@free.fr>
2017-04-05 22:39:25 +0200

    Add vagrant and rabbitmq

---
+++ setup.sh
@@ -0,0 +1,16 @@
+function InstallRabbitMq {
+  echo 'deb http://www.rabbitmq.com/debian/ testing main' | tee /etc/apt/sources.list.d/rabbitmq.list
+  wget -O- https://www.rabbitmq.com/rabbitmq-release-signing-key.asc | apt-key add
+  apt-get update
+  apt-get install --force-yes -y rabbitmq-server
+}
+
+function SetupRabbitMq {
+  rabbitmq-plugins enable rabbitmq_management
+  rabbitmqctl add_user root root
+  rabbitmqctl set_user_tags root administrator
+  rabbitmqctl set_permissions -p / root ".*" ".*" ".*"
+}
+
+echo 'Install RabbitMQ'; InstallRabbitMq
+echo 'Setup RabbitMQ'; SetupRabbitMq
